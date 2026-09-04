b1298c38e9ac0166c8ace5f260abefed9291bf9e
Aahan Mehta <mehta295@data.cs.purdue.edu>
2026-09-04 14:05:52 -0400

    Q2a: get rtts from traceroute on 5 random IPs
    Select 5 random lines from the file and run traceroute on those ip
    addresses. For every ip address' result, collect hop number and RTT
    metrics and print them.
    
    Signed-off-by: Aahan Mehta <mehta295@data.cs.purdue.edu>

--- latency_breakdown.py
+++ latency_breakdown.py
@@ -1,4 +1,9 @@
 import requests
+import csv
+import random
+import linecache
+import subprocess
+import re
 
 SERVER_LIST_URL = "https://export.iperf3serverlist.net/listed_iperf3_servers.csv"
 SERVER_LIST_FILE = "listed_iperf3_servers.csv"
@@ -18,4 +23,20 @@ def fetch_latest_server_list():
     return False
 
 fetch_latest_server_list()
+with open(SERVER_LIST_FILE) as file:
+  num_lines = sum(1 for line in file)
 
+# select 5 random line numbers and thus ip addresses by extension
+for line_number in random.sample(range(2, num_lines + 1), 5):
+  line = linecache.getline(SERVER_LIST_FILE, line_number).strip()
+  ip = line.split(',', 1)[0]
+  traceroute_result = subprocess.run(["traceroute", ip], capture_output=True, text=True)
+  for traceroute_line in traceroute_result.stdout.splitlines():
+    hop_number_part = traceroute_line.split()[0]
+    if not hop_number_part.isdigit():
+      continue
+    hop_number = int(hop_number_part)
+    rtts = re.findall(r"(\d+(?:\.\d+)?)\s*ms", traceroute_line) # find all instances of xx.xx ms to get rtt values
+    if len(rtts) == 0:
+      continue # skip unresponsive hops
+    print(ip, hop_number, rtts)
